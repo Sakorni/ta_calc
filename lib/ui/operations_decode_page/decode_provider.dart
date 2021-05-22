824ce884b2a578b67863820e6f6caf5513ec30c4
Sakorni <ssakorni@gmail.com>
2021-05-22 22:23:30 +0300

    I've forgot what i've changed sry

--- lib/ui/operations_decode_page/decode_provider.dart
+++ lib/ui/operations_decode_page/decode_provider.dart
@@ -4,8 +4,7 @@ import 'package:ta_calc/models/operations/abstract/reg_operation.dart';
 import 'package:ta_calc/resources/exceptions.dart';
 import 'package:ta_calc/ui/num_pad_delegate.dart';
 
-class DecodeProvider with ChangeNotifier implements NumPadDelegate {
-  String _buffer = "";
+class DecodeProvider with ChangeNotifier {
   Calculator _calc = Calculator();
   List<List<int>> codes = [];
   List<RegOpeartion> operations = [];
@@ -26,40 +25,6 @@ class DecodeProvider with ChangeNotifier implements NumPadDelegate {
   void reset() {
     codes.clear();
     operations.clear();
-    _buffer = "";
     notifyListeners();
-  }
-
-  @override
-  void addPressed() {
-    return;
-  }
-
-  @override
-  void calculate() {
-    if (_buffer.isNotEmpty) {
-      decode(
-        BigInt.parse(
-          _buffer.toString(),
-        ),
-      );
-    }
-  }
-
-  @override
-  void deleteAllPressed() {
-    reset();
-  }
-
-  @override
-  void deletePressed() {
-    if (_buffer.isNotEmpty) {
-      _buffer = _buffer.substring(0, _buffer.length - 1);
-    }
-  }
-
-  @override
-  void numberPressed(int number) {
-    _buffer += number.toString();
   }
 }
