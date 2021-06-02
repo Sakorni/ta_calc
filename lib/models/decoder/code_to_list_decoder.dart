014829794d0606533d5871c8b790addb74e06b00
Sakorni <ssakorni@gmail.com>
2021-06-02 19:51:21 +0300

    added second ui

--- lib/models/decoder/code_to_list_decoder.dart
+++ lib/models/decoder/code_to_list_decoder.dart
@@ -1,12 +1,17 @@
 import 'package:ta_calc/models/calculator.dart';
 import 'package:ta_calc/models/decoder/decoder_delegate.dart';
+import 'package:ta_calc/resources/exceptions.dart';
 
 class CodeToListDecoder implements DecoderDelegate {
   Calculator _calc = Calculator();
   @override
   String calc(String source) {
-    var bigInt = BigInt.tryParse(source) ?? BigInt.one;
-    var code = _calc.decode(bigInt);
-    return "$code\n";
+    try {
+      var bigInt = BigInt.tryParse(source) ?? BigInt.one;
+      var code = _calc.decode(bigInt);
+      return "$code\n";
+    } on InvalidCode catch (e) {
+      return e.message;
+    }
   }
 }
---
+++ lib/ui/second_ui/second_provider.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:ta_calc/models/decoder/code_to_list_decoder.dart';
+import 'package:ta_calc/models/decoder/list_to_code_decoder.dart';
+
+class SecondProvider with ChangeNotifier {
+  String str = "";
+  String ans = "";
+  ListToCodeDecoder listCodeDecoder = ListToCodeDecoder();
+  CodeToListDecoder codeToListDecoder = CodeToListDecoder();
+  void buttonPressed(String button) {
+    int? t = int.tryParse(button);
+    if (t != null) {
+      str += button;
+    } else {
+      switch (button) {
+        case "%":
+          break;
+        case "CE":
+          str = "";
+          break;
+        case "C":
+          str = "";
+          break;
+        case "⌫":
+          str = str.substring(0, str.length - 1);
+          break;
+        case "1/x":
+          break;
+        case "^":
+          break;
+        case "⌫":
+          break;
+        case "√":
+          break;
+        case "/":
+          break;
+        case "×":
+          break;
+        case "-":
+          break;
+        case "+":
+          break;
+        case ",":
+          if (!str.endsWith(",") & (str.isNotEmpty)) {
+            str += ",";
+          }
+          break;
+        case "=":
+          break;
+        case "listToCode":
+          ans = listCodeDecoder.calc(str);
+          break;
+        case "codeToList":
+          ans = codeToListDecoder.calc(str);
+          break;
+        case "codeToOpers":
+          break;
+      }
+    }
+    notifyListeners();
+  }
+}
